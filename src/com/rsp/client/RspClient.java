539d0068d2248243d8c0f70934fe3af0f8d0d728
parker <qkrrudtjr954@kangwon.ac.kr>
2017-10-15 11:49:55 +0900

    logic complete with server

--- src/com/rsp/client/RspClient.java
+++ src/com/rsp/client/RspClient.java
@@ -12,58 +12,61 @@ public class RspClient {
         client.connecting();
     }
     public void connecting(){
-        Socket socket = null;
-        OutputStream os = null;
-        OutputStreamWriter osw = null;
-        BufferedWriter bw = null;
+        while(true){
 
-        InputStream is = null;
-        InputStreamReader isr = null;
-        BufferedReader br = null;
+            Socket socket = null;
+            OutputStream os = null;
+            OutputStreamWriter osw = null;
+            BufferedWriter bw = null;
 
-        User user = new User();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("나이: ");
-        user.setAge(scanner.nextInt());
-        System.out.print("이름: ");
-        user.setName(scanner.next());
-        System.out.print("가위:1 바위:2 보:3");
-        user.setRsp(scanner.nextInt());
+            InputStream is = null;
+            InputStreamReader isr = null;
+            BufferedReader br = null;
 
-        String serverIP = "localhost";
-        int port = 3000;
+            User user = new User();
+            Scanner scanner = new Scanner(System.in);
+//        System.out.print("나이: ");
+//        user.setAge(scanner.nextInt());
+//        System.out.print("이름: ");
+//        user.setName(scanner.next());
+            System.out.println("가위:1 바위:2 보:3");
+            user.setRsp(scanner.nextInt());
 
-        try{
-            socket = new Socket(serverIP, port);
+            String serverIP = "localhost";
+            int port = 3000;
 
-            os = socket.getOutputStream();
-            osw = new OutputStreamWriter(os);
-            bw = new BufferedWriter(osw);
-
-            bw.write(user.getRsp());
-            bw.flush();
-
-            is = socket.getInputStream();
-            isr = new InputStreamReader(is);
-            br = new BufferedReader(isr);
-
-            String result = br.readLine();
-            System.out.println("Response from server: "+result);
-        }catch(IOException ex){
-            ex.printStackTrace();
-        } finally{
             try{
-                br.close();
-                isr.close();
-                is.close();
+                socket = new Socket(serverIP, port);
 
-                bw.close();
-                osw.close();
-                os.close();
+                os = socket.getOutputStream();
+                osw = new OutputStreamWriter(os);
+                bw = new BufferedWriter(osw);
 
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                bw.write(user.getRsp());
+                bw.flush();
+
+                is = socket.getInputStream();
+                isr = new InputStreamReader(is);
+                br = new BufferedReader(isr);
+
+                String result = br.readLine();
+                System.out.println("Response from server: "+result);
+            }catch(IOException ex){
+                ex.printStackTrace();
+            } finally{
+                try{
+                    br.close();
+                    isr.close();
+                    is.close();
+
+                    bw.close();
+                    osw.close();
+                    os.close();
+
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
